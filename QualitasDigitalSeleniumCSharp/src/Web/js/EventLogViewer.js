918db5e3d92eac0fd9079ac8c619a27855f96fe6
n-fowler <nfowler587@gmail.com>
2019-12-19 18:27:01 -0500

    change the way data is loaded for the event log table

--- QualitasDigitalSeleniumCSharp/src/Web/js/EventLogViewer.js
+++ QualitasDigitalSeleniumCSharp/src/Web/js/EventLogViewer.js
@@ -1,35 +1,80 @@
-﻿var placeholderDetailOne = {
-    "Detail": "Placeholder Event Log Details One"
-}
-var placeholderDetailTwo = {
-    "Detail": "Placeholder Event Log Details Two"
-}
-var placeholderDetailThree = {
-    "Detail": "Placeholder Event Log Details Three"
-}
-var placeholderDetailFour = {
-    "Detail": "Placeholder Event Log Details Four"
-}
-var placeholderDetailFive = {
-    "Detail": "Placeholder Event Log Details Five"
-}
-var placeholderDetailSix = {
-    "Detail": "Placeholder Event Log Details Six"
-}
-
-var placeholderDetailArray =
+﻿var EventData =
 [
-    placeholderDetailOne,
-    placeholderDetailTwo,
-    placeholderDetailThree,
-    placeholderDetailFour,
-    placeholderDetailFive,
-    placeholderDetailSix
-    ];
+    {
+        "Level": "Level Placeholder",
+        "DateAndTime": "Date and Time Placeholder",
+        "Source": "Source Placeholder",
+        "Task Category": "Task Category Placeholder"
+    },
+    {
+        "Level": "Level Placeholder",
+        "DateAndTime": "Date and Time Placeholder",
+        "Source": "Source Placeholder",
+        "Task Category": "Task Category Placeholder"
+    },
+    {
+        "Level": "Level Placeholder",
+        "DateAndTime": "Date and Time Placeholder",
+        "Source": "Source Placeholder",
+        "Task Category": "Task Category Placeholder"
+    },
+    {
+        "Level": "Level Placeholder",
+        "DateAndTime": "Date and Time Placeholder",
+        "Source": "Source Placeholder",
+        "Task Category": "Task Category Placeholder"
+    },
+    {
+        "Level": "Level Placeholder",
+        "DateAndTime": "Date and Time Placeholder",
+        "Source": "Source Placeholder",
+        "Task Category": "Task Category Placeholder"
+    },
+    {
+        "Level": "Level Placeholder",
+        "DateAndTime": "Date and Time Placeholder",
+        "Source": "Source Placeholder",
+        "Task Category": "Task Category Placeholder"
+    }
+];
 
-var detailArray = [];
+var detailData =
+[
+    {
+        "Details": "Event Details Placeholder One"
+    },
+    {
+        "Details": "Event Details Placeholder Two"
+    },
+    {
+        "Details": "Event Details Placeholder Three"
+    },
+    {
+        "Details": "Event Details Placeholder Four"
+    },
+    {
+        "Details": "Event Details Placeholder Five"
+    },
+    {
+        "Details": "Event Details Placeholder Six"
+    }
+];
+function addDataToTbody(nl, data) { // nl -> NodeList, data -> array with objects
+    data.forEach((d, i) => {
+        var tr = nl.insertRow(i);
+        Object.keys(d).forEach((k, j) => { // Keys from object represent th.innerHTML
+            tr.setAttribute("onclick", "FillDetails(this.rowIndex-1);");
+            var cell = tr.insertCell(j);
+            cell.innerHTML = d[k]; // Assign object values to cells   
+        });
+        nl.appendChild(tr);
+    });
+}
+
+var eventTableBody = document.querySelector("#EventLogTable");
+
+addDataToTbody(eventTableBody, EventData);
 
 function FillDetails(index) {
-    //window.document.getElementById("EventLogDetails").value = placeholderDetailArray[index].Detail;
-    window.document.getElementById("EventLogDetails").value = detailArray[index].Detail;
+    window.document.querySelector("#EventLogDetails").value = detailData[index].Details;
 }
