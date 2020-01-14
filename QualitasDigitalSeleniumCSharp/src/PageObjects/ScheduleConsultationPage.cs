cab363a5fb402bb09b7dfa96a015b94420766e33
n-fowler <nfowler587@gmail.com>
2020-01-14 17:47:06 -0500

    completed schedule consultation tests

--- QualitasDigitalSeleniumCSharp/src/PageObjects/ScheduleConsultationPage.cs
+++ QualitasDigitalSeleniumCSharp/src/PageObjects/ScheduleConsultationPage.cs
@@ -1,9 +1,75 @@
-﻿namespace QualitasDigitalSeleniumCSharp.PageObjects
+﻿using OpenQA.Selenium;
+using QualitasDigitalSeleniumCSharp.Extensions;
+using QualitasDigitalSeleniumCSharp.src.LocalConfiguration;
+using QualitasDigitalSeleniumCSharp.WrapperFactory;
+using System.Linq;
+
+namespace QualitasDigitalSeleniumCSharp.PageObjects
 {
     /// <summary>
     /// The ScheduleConsultation Page Object
     /// </summary>
     public class ScheduleConsultationPage
     {
+        private IWebDriver driver = BrowserFactory.Driver;
+
+        /// <summary>
+        /// The Pricing and Services Url
+        /// </summary>
+        public const string Url = "https://www.qualitasdigital.com/schedule-consultation";
+
+        #region Elements
+
+        /// <summary>
+        /// The Logo Image element
+        /// </summary>
+        public IWebElement LogoImage => driver.FindElementByClassName("logo-image");
+
+        /// <summary>
+        /// Header Section element
+        /// </summary>
+        public IWebElement HeaderSection => driver.FindElementById("block-540803d1406a17b6338b");
+
+        /// <summary>
+        /// Header Content element
+        /// </summary>
+        public IWebElement HeaderContent => HeaderSection.FindElementByTagName("div").FindElementByTagName("h1");
+
+        /// <summary>
+        /// Left Section element
+        /// </summary>
+        public IWebElement LeftSection => driver.FindElementById("block-6bcc55f9ff3b7b21fdea");
+
+        /// <summary>
+        /// Left Content element
+        /// </summary>
+        public IWebElement LeftContent => LeftSection.FindElementByTagName("div").FindElementByTagName("h2");
+
+        /// <summary>
+        /// Our Commitment Link element
+        /// </summary>
+        public IWebElement OurCommitmentLink => LeftSection.FindElementByTagName("div").FindElementByTagName("p").FindElementsByTagName("a").ToList()[0];
+
+        /// <summary>
+        /// Faq Link element
+        /// </summary>
+        public IWebElement FaqLink => LeftSection.FindElementByTagName("div").FindElementByTagName("p").FindElementsByTagName("a").ToList()[1];
+
+        /// <summary>
+        /// Terms And Conditions Link element
+        /// </summary>
+        public IWebElement TermsAndConditionsLink => LeftSection.FindElementByTagName("div").FindElementByTagName("p").FindElementsByTagName("a").ToList()[2];
+
+        /// <summary>
+        /// Schedule Consultation Button element
+        /// </summary>
+        public IWebElement ScheduleConsultationButton => driver.GetElementsWithTagAndAttributeStartAndEndingWithValues("div", "id", Configuration.SitePrefix, "68").First();
+
+        /// <summary>
+        /// Schedule Consultation Button Link element
+        /// </summary>
+        public IWebElement ScheduleConsultationButtonLink => ScheduleConsultationButton.FindElementByTagName("a");
+
+        #endregion Elements
     }
 }
--- QualitasDigitalSeleniumCSharp/src/TestCases/ScheduleConsultationTests.cs
+++ QualitasDigitalSeleniumCSharp/src/TestCases/ScheduleConsultationTests.cs
@@ -1,6 +1,98 @@
-﻿namespace QualitasDigitalSeleniumCSharp.TestCases
+﻿using NUnit.Framework;
+using QualitasDigitalSeleniumCSharp.Extensions;
+using QualitasDigitalSeleniumCSharp.PageObjects;
+using QualitasDigitalSeleniumCSharp.src.TestData;
+using QualitasDigitalSeleniumCSharp.WrapperFactory;
+using System;
+
+namespace QualitasDigitalSeleniumCSharp.TestCases
 {
-    class ScheduleConsultationTests
+    /// <summary>
+    /// Schedule Consultation Tests
+    /// </summary>
+    public class ScheduleConsultationTests
     {
+        #region Fields
+
+        private BrowserFactory.WebDriver webDriverEnum = BrowserFactory.WebDriver.Chrome;
+
+        #endregion Fields
+
+        #region Shared
+
+        /// <summary>
+        /// The one time test setup method
+        /// </summary>
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            BrowserFactory.InitBrowser(webDriverEnum);
+        }
+
+        /// <summary>
+        /// The one time test tear down method
+        /// </summary>
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            BrowserFactory.CloseAllDrivers();
+        }
+
+        /// <summary>
+        /// The individual test setup method
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            Logging.SetTestRunId();
+            Logging.TestStartTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// The individual test tear down method
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            Logging.TestEndTime = DateTime.Now;
+            BrowserFactory.ReportTestStatus(TestContext.CurrentContext);
+        }
+
+        #endregion Shared
+
+        #region Tests
+
+        /// <summary></summary>
+        [Test]
+        public void ScheduleConsultationLoad()
+        {
+            BrowserFactory.GoToPage(ScheduleConsultationPage.Url);
+            BrowserFactory.WaitForPageLoad(10);
+            Assert.IsTrue(Page.ScheduleConsultation.LogoImage.Displayed, "Page.ScheduleConsultation.LogoImage.Displayed");
+        }
+
+        /// <summary></summary>
+        [Test]
+        public void ScheduleConsultationContent()
+        {
+            BrowserFactory.GoToPage(ScheduleConsultationPage.Url);
+            BrowserFactory.WaitForPageLoad(10);
+
+            //Validate Title
+            Assert.AreEqual(ScheduleConsultationPageData.HeaderContent, Page.ScheduleConsultation.HeaderContent.GetInnertext());
+
+            //Validate left section
+            Assert.AreEqual(ScheduleConsultationPageData.LeftContent, Page.ScheduleConsultation.LeftContent.GetInnertext());
+            Assert.AreEqual(ScheduleConsultationPageData.OurCommitmentLink, Page.ScheduleConsultation.OurCommitmentLink.GetHref());
+            Assert.AreEqual(ScheduleConsultationPageData.FaqLink, Page.ScheduleConsultation.FaqLink.GetHref());
+            Assert.AreEqual(ScheduleConsultationPageData.TermsAndConditionsLink, Page.ScheduleConsultation.TermsAndConditionsLink.GetHref());
+
+            //Validate right section
+            Assert.AreEqual(ScheduleConsultationPageData.ScheduleConsultationButtonLink, Page.ScheduleConsultation.ScheduleConsultationButtonLink.GetHref());
+        }
+
+        //Due to the lack of test environments, there aren't any tests for scheduling the consultation on calendly, and their tests cover that anyway.
+
+        #endregion Tests
     }
 }
