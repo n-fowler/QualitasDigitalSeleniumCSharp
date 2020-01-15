075aa810699868310c837510831f36536e3881b1
n-fowler <nfowler587@gmail.com>
2020-01-14 19:04:56 -0500

    completed contact us tests

--- QualitasDigitalSeleniumCSharp/src/PageObjects/ContactUsPage.cs
+++ QualitasDigitalSeleniumCSharp/src/PageObjects/ContactUsPage.cs
@@ -1,9 +1,100 @@
-﻿namespace QualitasDigitalSeleniumCSharp.PageObjects
+﻿using OpenQA.Selenium;
+using QualitasDigitalSeleniumCSharp.Extensions;
+using QualitasDigitalSeleniumCSharp.src.LocalConfiguration;
+using QualitasDigitalSeleniumCSharp.WrapperFactory;
+using System.Linq;
+
+namespace QualitasDigitalSeleniumCSharp.PageObjects
 {
     /// <summary>
     /// The ContactUs Page Object
     /// </summary>
     public class ContactUsPage
     {
+        private IWebDriver driver = BrowserFactory.Driver;
+
+        /// <summary>
+        /// The Pricing and Services Url
+        /// </summary>
+        public const string Url = "https://www.qualitasdigital.com/contact-us";
+
+        #region Elements
+
+        /// <summary>
+        /// The Logo Image element
+        /// </summary>
+        public IWebElement LogoImage => driver.FindElementByClassName("logo-image");
+
+        /// <summary>
+        /// Header Section element
+        /// </summary>
+        public IWebElement HeaderSection => driver.FindElementById("block-66e658da5d8a61ea0d14");
+
+        /// <summary>
+        /// Header Content element
+        /// </summary>
+        public IWebElement HeaderContent => HeaderSection.FindElementByTagName("div").FindElementByTagName("h1");
+
+        /// <summary>
+        /// Left Section element
+        /// </summary>
+        public IWebElement LeftSection => driver.FindElementById("block-ef19513355911ef99e33");
+
+        /// <summary>
+        /// Left Content element
+        /// </summary>
+        public IWebElement LeftContent => LeftSection.FindElementByTagName("div").FindElementByTagName("h2");
+
+        /// <summary>
+        /// Our Commitment Link element
+        /// </summary>
+        public IWebElement OurCommitmentLink => LeftSection.FindElementByTagName("div").FindElementByTagName("p").FindElementsByTagName("a").ToList()[0];
+
+        /// <summary>
+        /// Faq Link element
+        /// </summary>
+        public IWebElement FaqLink => LeftSection.FindElementByTagName("div").FindElementByTagName("p").FindElementsByTagName("a").ToList()[1];
+
+        /// <summary>
+        /// Terms And Conditions Link element
+        /// </summary>
+        public IWebElement TermsAndConditionsLink => LeftSection.FindElementByTagName("div").FindElementByTagName("p").FindElementsByTagName("a").ToList()[2];
+
+        /// <summary>
+        /// Form Section element
+        /// </summary>
+        public IWebElement FormSection => driver.FindElementById("block-1d7f70b9528dd074da0a");
+
+        /// <summary>
+        /// First Name Field element
+        /// </summary>
+        public IWebElement FirstNameField => FormSection.FindElementByName("fname");
+
+        /// <summary>
+        /// Last Name Field element
+        /// </summary>
+        public IWebElement LastNameField => FormSection.FindElementByName("lname");
+
+        /// <summary>
+        /// Email Field element
+        /// </summary>
+        public IWebElement EmailField => FormSection.FindElementByName("email");
+
+        /// <summary>
+        /// Checkbox Section element
+        /// </summary>
+        public IWebElement CheckboxSection => FormSection.GetElementsWithTagAndAttributeStartAndEndingWithValues("div", "id", "checkbox-" + Configuration.SitePrefix, "22390").First();
+
+        /// <summary>
+        /// Additional Message Text Area element
+        /// </summary>
+        public IWebElement AdditionalMessageTextArea => FormSection.GetElementsWithTagAndAttributeStartAndEndingWithValues("textarea", "id", "textarea-" + Configuration.SitePrefix, "67663-field").First();
+
+        /// <summary>
+        /// Submit Button element
+        /// </summary>
+        public IWebElement SubmitButton => FormSection.FindElementByClassName("form-button-wrapper").FindElementByTagName("input");
+
+        #endregion Elements
     }
 }
--- QualitasDigitalSeleniumCSharp/src/TestCases/ContactUsTests.cs
+++ QualitasDigitalSeleniumCSharp/src/TestCases/ContactUsTests.cs
@@ -1,6 +1,160 @@
-﻿namespace QualitasDigitalSeleniumCSharp.TestCases
+﻿using NUnit.Framework;
+using QualitasDigitalSeleniumCSharp.Extensions;
+using QualitasDigitalSeleniumCSharp.PageObjects;
+using QualitasDigitalSeleniumCSharp.src.TestData;
+using QualitasDigitalSeleniumCSharp.WrapperFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QualitasDigitalSeleniumCSharp.TestCases
 {
-    class ContactUsTests
+    /// <summary>
+    /// Contact Us Tests
+    /// </summary>
+    public class ContactUsTests
     {
+        #region Fields
+
+        private BrowserFactory.WebDriver webDriverEnum = BrowserFactory.WebDriver.Chrome;
+
+        #endregion Fields
+
+        #region Shared
+
+        /// <summary>
+        /// The one time test setup method
+        /// </summary>
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            BrowserFactory.InitBrowser(webDriverEnum);
+        }
+
+        /// <summary>
+        /// The one time test tear down method
+        /// </summary>
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            BrowserFactory.CloseAllDrivers();
+        }
+
+        /// <summary>
+        /// The individual test setup method
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            Logging.SetTestRunId();
+            Logging.TestStartTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// The individual test tear down method
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            Logging.TestEndTime = DateTime.Now;
+            BrowserFactory.ReportTestStatus(TestContext.CurrentContext);
+        }
+
+        #endregion Shared
+
+        #region Tests
+
+        /// <summary></summary>
+        [Test]
+        public void ContactUsLoad()
+        {
+            BrowserFactory.GoToPage(ContactUsPage.Url);
+            BrowserFactory.WaitForPageLoad(10);
+            Assert.IsTrue(Page.ContactUs.LogoImage.Displayed, "Page.ContactUs.LogoImage.Displayed");
+        }
+
+        /// <summary></summary>
+        [Test]
+        public void ContactUsContent()
+        {
+            BrowserFactory.GoToPage(ContactUsPage.Url);
+            BrowserFactory.WaitForPageLoad(10);
+
+            //Validate Title
+            Assert.AreEqual(ContactUsPageData.HeaderContent, Page.ContactUs.HeaderContent.GetInnertext());
+
+            //Validate left section
+            Assert.AreEqual(ContactUsPageData.LeftContent, Page.ContactUs.LeftContent.GetInnertext());
+            Assert.AreEqual(ContactUsPageData.OurCommitmentLink, Page.ContactUs.OurCommitmentLink.GetHref());
+            Assert.AreEqual(ContactUsPageData.FaqLink, Page.ContactUs.FaqLink.GetHref());
+            Assert.AreEqual(ContactUsPageData.TermsAndConditionsLink, Page.ContactUs.TermsAndConditionsLink.GetHref());
+
+            //Validate right section
+
+            //There should be a first name field
+            Assert.NotNull(Page.ContactUs.FirstNameField, "Page.ContactUs.FirstNameField != null");
+
+            //There should be a last name field
+            Assert.NotNull(Page.ContactUs.LastNameField, "Page.ContactUs.LastNameField != null");
+
+            //There should be an email field
+            Assert.NotNull(Page.ContactUs.EmailField, "Page.ContactUs.EmailField != null");
+
+            /*There should be a series of checkboxes with the following organization: 
+              I'm interested in help with...
+                Additional Test Coverage
+                Framework Updates
+                Process Automation
+                Dashboards
+                Training
+                Coaching
+                Road maps
+                Trusted Advisor
+                Technical Co-Founder
+                Other* (Include additional detail below)
+            */
+
+            var options = Page.ContactUs.CheckboxSection.FindElementsByTagName("div").ToList();
+            var expectedOptions = new List<string>
+            {
+                "I’m interested in help with ... *",
+                "Additional Test Coverage",
+                "Framework Updates",
+                "Process Automation",
+                "Dashboards",
+                "Training",
+                "Coaching",
+                "Road maps",
+                "Trusted Advisor",
+                "Technical Co-Founder",
+                "Other* (Include additional detail below)"
+            };
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (i == 0)
+                {
+                    //Verify the title
+                    Assert.AreEqual("title", options[i].GetAttribute("class"));
+                    Assert.AreEqual(expectedOptions[i], options[i].GetInnertext().Trim());
+                }
+                else
+                {
+                    //Verify the checkbox
+                    Assert.AreEqual("checkbox", options[i].FindElementByTagName("label").FindElementByTagName("input").GetProperty("type"));
+                    Assert.AreEqual(expectedOptions[i], options[i].FindElementByTagName("label").FindElementByTagName("input").GetProperty("value"));
+                }
+            }
+
+            //There should be an additional message field
+            Assert.NotNull(Page.ContactUs.AdditionalMessageTextArea, "Page.ContactUs.AdditionalMessageTextArea != null");
+
+            //There should be a submit button
+            Assert.NotNull(Page.ContactUs.SubmitButton, "Page.ContactUs.SubmitButton != null");
+            Assert.AreEqual("submit", Page.ContactUs.SubmitButton.GetProperty("type"));
+            Assert.AreEqual("Submit", Page.ContactUs.SubmitButton.GetProperty("value"));
+        }
+
+        #endregion Tests
     }
 }
